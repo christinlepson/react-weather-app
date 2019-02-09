4a28fe346b5593b61c67f82f656b03834678e0ec
Christin Nadya Lepson <christin@protonmail.com>
2019-02-09 13:26:11 -0500

    Updates

---
+++ precache-manifest.b521f97b7671ff9d690322e79ed6cf02.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6368745ceb7bb2bb3d25df4bff18c08d",
+    "url": "/react-weather-app/static/media/partly-cloudy-night.6368745c.jpg"
+  },
+  {
+    "revision": "2d3a98fc20b8d7648c53",
+    "url": "/react-weather-app/static/css/main.7912e786.chunk.css"
+  },
+  {
+    "revision": "53afdbdb4064c022a51b",
+    "url": "/react-weather-app/static/js/1.53afdbdb.chunk.js"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "e12849a5eb5ed58221b7e74c7243fd96",
+    "url": "/react-weather-app/static/media/error.e12849a5.svg"
+  },
+  {
+    "revision": "1bb9825b703d91fcb8af80c91a6a37ac",
+    "url": "/react-weather-app/static/media/cloudy.1bb9825b.jpg"
+  },
+  {
+    "revision": "f3185bceb2edb3140fc00f0ace97d6c8",
+    "url": "/react-weather-app/static/media/partly-cloudy-day.f3185bce.jpg"
+  },
+  {
+    "revision": "72115c6352cf4ec8c86290388b768cc5",
+    "url": "/react-weather-app/static/media/fog.72115c63.jpg"
+  },
+  {
+    "revision": "2d3a98fc20b8d7648c53",
+    "url": "/react-weather-app/static/js/main.2d3a98fc.chunk.js"
+  },
+  {
+    "revision": "95b8fa1bb074e3ee9efe774078a541e5",
+    "url": "/react-weather-app/static/media/clear-night.95b8fa1b.jpg"
+  },
+  {
+    "revision": "fc5a568d0d509d1abdaaaaf272b9f9c9",
+    "url": "/react-weather-app/static/media/wind.fc5a568d.jpg"
+  },
+  {
+    "revision": "1911515da75efa0fcdb07379b576bcab",
+    "url": "/react-weather-app/static/media/sleet.1911515d.jpg"
+  },
+  {
+    "revision": "c0e59c2a3dedefae9e01813f0a3d1cf4",
+    "url": "/react-weather-app/static/media/rain.c0e59c2a.jpg"
+  },
+  {
+    "revision": "f0859cc411d4cbde77c575b0c9135795",
+    "url": "/react-weather-app/static/media/clear-day.f0859cc4.jpg"
+  },
+  {
+    "revision": "8105bd2635f36ec0a7fb5c265471087b",
+    "url": "/react-weather-app/static/media/snow.8105bd26.jpg"
+  },
+  {
+    "revision": "00761b0f10ab17aea2bea0ac56e8c8b0",
+    "url": "/react-weather-app/index.html"
+  }
+];
